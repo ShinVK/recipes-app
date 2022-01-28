9a69c727301041614a6dffe293a9d944d7238007
shinvk <victor.kamiguchi@gmail.com>
2022-01-28 16:35:25 -0300

    C13.2 feat radio butons in components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import rockGlass from './images/rockGlass.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchHeader from './components/SearchHeader';
 
 function App() {
   return (
     <div className="meals">
       <span className="logo">TRYBE</span>
+      <SearchHeader />
       <object
         className="rocksGlass"
         type="image/svg+xml"
--- src/components/SearchHeader.js
+++ src/components/SearchHeader.js
@@ -1,8 +1,40 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
-function SearchHeader(props) {
-  return <div className="teste" />;
+function SearchHeader() {
+  return (
+    <div>
+      <form>
+        <input
+          type="radio"
+          data-testid="ingredient-search-radio"
+          // value
+          // name
+        />
+        Ingredientes
+        <input
+          type="radio"
+          data-testid="name-search-radio"
+          // value
+          // name
+        />
+        Nome
+        <input
+          type="radio"
+          data-testid="first-letter-search-radio"
+          // value
+          // name
+        />
+        Primeira letra
+      </form>
+      <button
+        type="button"
+        data-testid="exec-search-btn"
+      >
+        Procurar
+      </button>
+    </div>
+  );
 }
 
 // SearchHeader.propTypes = {};
