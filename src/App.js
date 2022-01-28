52d8cc0b95d8a48e4a03a59160fba96d1c9c97ab
shinvk <victor.kamiguchi@gmail.com>
2022-01-28 16:58:24 -0300

    C14.2 FIX merge conflicts

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import SearchHeader from './components/SearchHeader';
 import Header from './components/Header';
 
-
 function App() {
   return (
     <div className="meals">
