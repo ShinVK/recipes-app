5506db71f58a898adab840afd5c38bbddee6a509
Lucas Sodre <lucasmeinen@yahoo.com.br>
2022-02-03 15:04:27 -0300

    pagina de explorar

--- src/pages/Explorar.js
+++ src/pages/Explorar.js
@@ -1,11 +1,29 @@
 import React from 'react';
-import AllHeader from '../components/AllHeader';
+// import AllHeader from '../components/AllHeader';
+
+import { useHistory } from 'react-router-dom';
 
 export default function Explorar() {
+  const history = useHistory();
+
   return (
     <div>
-      <AllHeader title="Explore" btnSearch={ false } />
-      <p>Tela Principal</p>
+      {/* <AllHeader title="Explore" btnSearch={ false } /> */}
+      {/* <p>Tela Principal</p> */}
+      <button
+        type="button"
+        data-testid="explore-foods"
+        onClick={ () => history.push('/explore/foods') }
+      >
+        Explore Foods
+      </button>
+      <button
+        type="button"
+        data-testid="explore-drinks"
+        onClick={ () => history.push('/explore/drinks') }
+      >
+        Explore Drinks
+      </button>
     </div>
   );
 }
