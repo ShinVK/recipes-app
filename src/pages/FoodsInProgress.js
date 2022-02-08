6fc4be469b090792ea65eedb5215aafadd9ea4fa
shinvk <victor.kamiguchi@gmail.com>
2022-02-08 18:20:45 -0300

    CSS Foods in progress

--- src/pages/FoodsInProgress.js
+++ src/pages/FoodsInProgress.js
@@ -1,14 +1,26 @@
+/* eslint-disable react/jsx-max-depth */
 import React, { useContext, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid, IconButton, Typography }
+from '@mui/material';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 import MyContext from '../context/Mycontext';
 import useUpdateDetailRecipe from '../hooks/useUpdateDetailRecipe';
-import blackHeartIcon from '../images/blackHeartIcon.svg';
-import whiteHeartIcon from '../images/whiteHeartIcon.svg';
 import useCatchIngredients from '../hooks/useCatchIngredients';
 import { saveRecipesDone, saveRecipesInProgess } from '../services/localStorage';
 import { foodsDone } from '../services/favRecipes';
 import useVerifyStatus from '../hooks/useVerifiyStatus';
+import ClipBoardCopy from '../components/mui/ClipBoardCopy';
+import AllHeader from '../components/AllHeader';
+import BottomNav from '../components/mui/BottomNav';
 
 function FoodsInProgress({ location: { pathname } }) {
   const {
@@ -16,8 +28,6 @@ function FoodsInProgress({ location: { pathname } }) {
     {
       isFavorite,
       handleClickFavorite,
-      isCopied,
-      copyClipBoard,
     } } = useContext(MyContext);
   const [detailItem, id] = useUpdateDetailRecipe(pathname, true);
   const [isLoading, setIsLoading] = useState(false);
@@ -59,76 +69,106 @@ function FoodsInProgress({ location: { pathname } }) {
   };
 
   return (
-    <div className="container">
-      { isLoading
-        ? (
-          <>
-            <img
-              src={ detailItem.strMealThumb }
-              alt="imagem da refeição"
-              data-testid="recipe-photo"
-              width="280"
-              height="155"
-            />
-            <h2 data-testid="recipe-title">{ detailItem.strMeal }</h2>
-            <h4 data-testid="recipe-category">{ detailItem.strCategory }</h4>
-            <button
-              type="button"
-              data-testid="share-btn"
-              className="btn btn-primary"
-              onClick={ () => copyClipBoard(`foods/${id}`) }
+    <>
+      <AllHeader
+        title="Favorite Recipes"
+        btnSearch={ false }
+        actPage={ pathname }
+      />
+      <Box sx={ { '@ sx': { minWidth: 350 }, mt: 3, mb: 10 } }>
+        { isLoading
+          ? (
+            <Grid
+              container
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              sx={ { '@ sx': { maxWidth: 340 } } }
             >
-              Compartilhar
-            </button>
-            { isCopied && <span style={ { font: '10px' } }>Link copied!</span> }
-            <input
-              data-testid="favorite-btn"
-              type="image"
-              src={ isFavorite ? blackHeartIcon : whiteHeartIcon }
-              alt="search icon"
-              onClick={ () => handleClickFavorite(id, detailItem, true) }
-            />
-            <p data-testid="instructions">
-              { detailItem.strInstructions }
-            </p>
+              <Grid item xs={ 3 } textAlign="left">
 
-            {ingredients.map((ingredient, i) => (
-              <div
-                className="form-check"
-                key={ i }
-                data-testid={ `${i}-ingredient-step` }
-              >
-                <label
-                  key={ i }
-                  className={ (steps[i])
-                    ? 'checked_text form-check-label' : 'form-check-label' }
-                  htmlFor={ `${i}-input` }
-                >
-
-                  <input
-                    type="checkbox"
-                    className="form-check-input"
-                    id={ `${i}-input` }
-                    name={ `step${i}` }
-                    checked={ steps[i] }
-                    onChange={ () => onHandleChange(i) }
+                <Card sx={ { maxWidth: 340 } }>
+                  <CardMedia
+                    component="img"
+                    minwidth="150"
+                    image={ detailItem.strMealThumb }
+                    alt="imagem da refeição"
+                    data-testid="recipe-photo"
                   />
-                  {ingredient}
-                </label>
-              </div>
-            ))}
+                  <CardContent>
+                    <Typography
+                      variant="h4"
+                      component="div"
+                      color="secondary"
+                      data-testid="recipe-title"
+                    >
+                      { detailItem.strMeal }
+                    </Typography>
+                  </CardContent>
+                  <CardActions disableSpacing>
+                    <IconButton
+                      onClick={ () => handleClickFavorite(id, detailItem, true) }
+                      aria-label="add to favorites"
+                    >
+                      <FavoriteIcon
+                        color={ isFavorite ? 'primary' : 'grey[500]' }
+                        data-testid="favorite-btn"
+                      />
+                    </IconButton>
+                    <ClipBoardCopy url={ `foods/${detailItem.idMeal}` } />
+                    <Button
+                      type="button"
+                      data-testid="finish-recipe-btn"
+                      disabled={ disable }
+                      onClick={ () => handleClickDone(detailItem) }
+                      variant="contained"
+                    >
+                      Done!
+                    </Button>
+                  </CardActions>
+                  <CardContent>
+                    <Typography
+                      variant="body2"
+                      component="div"
+                      color="secondary"
+                      data-testid="instructions"
+                    >
+                      { detailItem.strInstructions }
+                    </Typography>
+                    {ingredients.map((ingredient, i) => (
+                      <div
+                        className="form-check"
+                        key={ i }
+                        data-testid={ `${i}-ingredient-step` }
+                      >
+                        <label
+                          key={ i }
+                          className={ (steps[i])
+                            ? 'checked_text form-check-label' : 'form-check-label' }
+                          htmlFor={ `${i}-input` }
+                        >
 
-            <button
-              type="button"
-              data-testid="finish-recipe-btn"
-              disabled={ disable }
-              onClick={ () => handleClickDone(detailItem) }
-            >
-              Done!
-            </button>
-          </>
-        ) : <p>carregando...</p> }
-    </div>);
+                          <input
+                            type="checkbox"
+                            className="form-check-input"
+                            id={ `${i}-input` }
+                            name={ `step${i}` }
+                            checked={ steps[i] }
+                            onChange={ () => onHandleChange(i) }
+                          />
+                          {ingredient}
+                        </label>
+                      </div>
+                    ))}
+                  </CardContent>
+                </Card>
+              </Grid>
+            </Grid>
+          ) : <p>carregando...</p> }
+      </Box>
+      <BottomNav />
+    </>
+  );
 }
 
 FoodsInProgress.propTypes = {
